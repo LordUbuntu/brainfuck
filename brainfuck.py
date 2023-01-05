e3447c83b6d6777d4672bc22400b0b63a806bb0d
Jacobus Burger <therealjacoburger@gmail.com>
2023-01-04 20:18:48 -0800

    improve documentation for interpreter v2

--- brainfuck.py
+++ brainfuck.py
@@ -9,18 +9,18 @@ from functools import partial
 
 def main():
     # Turing Machine Emulator
-    tape = [0] * 30_000  # array of tapes (the tape)
-    head = 0  # current tape (the head)
-
-    # BF symbols
+    tape = [0] * 30_000  # array of cells each holding an ascii value [0..255]
+    head = 0  # current cell
+    # valid BF symbols/commands
     bf_commands = ['+', '-', '<', '>', ',', '.', '[', ']', '#']
 
 
     # read program from given file
     try:
         with open(argv[1], "r") as file:
-            # filter file for recognized BF commands
+            # filter file for recognized BF commands only
             bf_program_filter = partial(contains, bf_commands)
+            # NOTE: program is an array of BF symbols/commands
             program = list(filter(bf_program_filter, file.read()))
     # handle potential exceptions
     except IndexError:
@@ -29,7 +29,7 @@ def main():
         exit(f"file '{argv[1]}' could not be found!")
 
 
-    # validate the program by ensuring no unmatched brackets
+    # validate the program by ensuring no unmatched brackets as precondition
     bracket_symmetry = program.count('[') - program.count(']')
     if program.count('[') - program.count('[') != 0:
         exit("ERR: unmatched '[' or ']' in program")
@@ -51,15 +51,24 @@ def main():
 
     # run the program
     index = 0  # the index of the current command in the program
-    skip = 0  # used to determine if and in which direction to skip
+    skip = 0  # used to determine if and in which direction to loop [ and ]
     while index < len(program):
         # grab current command in program
         command = program[index]
 
         # execute the current command in the program
         if skip != 0:
-            # skip to a matching bracket
+            # skip to a matching bracket if skip is active
+            # what is depth and why are we using it?
+            #   the [ and ] skip algorithm is pretty straightforward. Treat
+            #   all [ like a 1 and all ] like a -1, then adding the values of
+            #   the matching pairs will give us 0 (1 - 1 == 0). This way we
+            #   can ignore commands and nested [ and ] between the start [
+            #   and its matching ] assuming we're skipping forward. The logic
+            #   to go backwards is the same but we just reverse the values
+            #   so that [ is -1 and ] is 1.
             if skip == 1:
+                # skip forward...
                 # start on the command following calling [
                 index += 1
                 # skip forwards to command after matching ]
@@ -75,6 +84,7 @@ def main():
                 skip = 0
                 continue
             if skip == -1:
+                # skip backwards...
                 # start on the command before the calling ]
                 index -= 1
                 # skip backwards to the command after matching [
